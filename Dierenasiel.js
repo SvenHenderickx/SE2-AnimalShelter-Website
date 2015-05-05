f869656a2fe61432931525b1c029f757630d0dac
AlexRasSoftware <alexraswow@outlook.com>
2015-05-05 19:30:02 +0200

    11.7.3 en 11.7.4
    - form een naam gegeven
    - controle gemaakt vor het form wanneer er op toevoegen wordt geklikt

--- Dierenasiel.js
+++ Dierenasiel.js
@@ -2,3 +2,14 @@ function OnReady() {
   console.log('Pagina geladen, DOM klaar voor gebruik.');
 }
 $(document).ready(OnReady);
+
+function validateForm() {
+    var x = document.forms["myForm"]["naam"].value;
+	var y = document.forms["myForm"]["leeftijd"].value;
+	var r = document.forms["myForm"]["regnummer"].value;
+	var i = r.indexOf("0");
+    if (x == null || x == "" || y < 0 || i > 0) {
+        alert("Naam is verplicht, leeftijd moet 0 of hoger zijn en registratienummer moet met een 0 beginnen!");
+        return false;
+    }
+}
